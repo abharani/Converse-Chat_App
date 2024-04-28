57354b309e06e6220df2808a15547973f3006a17
Abha Rani <abha.rangira@gmail.com>
2024-04-28 21:09:26 +0530

    files uploading

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Fronted
+          </div>
+  );
+}
+
+export default App;
